f12b86aec6cd88256b3274e4012f742756b751ee
Albin Johansson <albinoscar99@hotmail.com>
2020-11-04 23:48:54 +0100

    Add skeleton

--- include/abby.hpp
+++ include/abby.hpp
@@ -23,13 +23,107 @@
  *
  */
 
+#include <map>       // map
+#include <optional>  // optional
+#include <vector>    // vector
+
 #ifndef ABBY_HEADER_GUARD
 #define ABBY_HEADER_GUARD
 
 namespace abby {
 
+using opt_int = std::optional<int>;
 
+template <typename T>
+struct vec2 final
+{
+  T x{};
+  T y{};
+};
 
-}
+template <typename T = float>
+struct aabb final
+{
+  vec2<T> min;
+  vec2<T> max;
+};
+
+/**
+ * \struct aabb_node
+ *
+ * \brief Represents a node in an AABB tree.
+ *
+ * \details Contains an AABB and the entity associated with the AABB, along
+ * with tree information.
+ *
+ * \headerfile abby.hpp
+ */
+template <typename T, typename U = float>
+struct aabb_node final
+{
+  T id;
+  aabb<U> box;
+  opt_int parent;
+  opt_int left;
+  opt_int right;
+  opt_int next;
+};
+
+template <typename Key, typename T = float>
+class aabb_tree final
+{
+ public:
+  using key_type = Key;
+  using size_type = std::size_t;
+  using aabb_type = aabb<T>;
+  using vector_type = vec2<T>;
+  using index_type = int;
+
+  void insert(const key_type& key, const aabb_type& box)
+  {
+    
+  }
+
+  void remove(const key_type& key)
+  {}
+
+  void update(const key_type& key, const aabb_type& box)
+  {}
+
+  void move(const key_type& key, const vector_type& offset)
+  {}
+
+  template <typename OutputIterator>
+  void query_collisions(const key_type& key, OutputIterator iterator) const
+  {}
+
+  [[nodiscard]] auto get_aabb(const key_type& key) const -> const aabb_type&
+  {
+    return m_nodes.at(m_indexMap.at(key)).box;
+  }
+
+  [[nodiscard]] auto size() const noexcept -> size_type
+  {
+    return m_indexMap.size();
+  }
+
+  [[nodiscard]] auto is_empty() const noexcept -> bool
+  {
+    return m_indexMap.empty();
+  }
+
+ private:
+  std::map<key_type, index_type> m_indexMap;
+  std::vector<aabb_type> m_nodes;
+
+  std::optional<index_type> m_rootIndex{};
+  std::optional<index_type> m_nextFreeNodeIndex{};
+
+  size_type m_allocatedNodes{};
+  size_type m_nodeCapacity{24};
+  size_type m_growthSize{m_nodeCapacity};
+};
+
+}  // namespace abby
 
 #endif  // ABBY_HEADER_GUARD
